30a9f946b0f21fa34b14d4047fdcd5b0e200b845
Idra Hsieh <idra.hsieh@gmail.com>
2025-10-23 14:38:42 +0800

    feat: alert when check is clicked with empty input

--- freeCodeCamp/02-javascript-algorithms-and-data-structures/01-palindrome-hecker/script.js
+++ freeCodeCamp/02-javascript-algorithms-and-data-structures/01-palindrome-hecker/script.js
@@ -1,1 +1,11 @@
-console.log("Palindrome Checker ready");
+const inputEl = document.getElementById("text-input")
+const checkBtn = document.getElementById("check-btn")
+const resultEl = document.getElementById("result")
+
+checkBtn.addEventListener("click", () => {
+    const raw = inputEl.ariaValueMax.trim();
+    if (!raw) {
+        alert("Please input a value.");
+        return;
+    }
+})
