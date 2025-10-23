7651f29fa37251f0967e647c144aef556b7fe665
Idra Hsieh <idra.hsieh@gmail.com>
2025-10-23 15:03:58 +0800

    feat: implement normalize + palindrome check and render result

--- freeCodeCamp/02-javascript-algorithms-and-data-structures/01-palindrome-hecker/script.js
+++ freeCodeCamp/02-javascript-algorithms-and-data-structures/01-palindrome-hecker/script.js
@@ -2,10 +2,26 @@ const inputEl = document.getElementById("text-input")
 const checkBtn = document.getElementById("check-btn")
 const resultEl = document.getElementById("result")
 
-checkBtn.addEventListener("click", () => {
-    const raw = inputEl.ariaValueMax.trim();
-    if (!raw) {
+// remove all non-alphanumeric characters and lowercase
+function normalize(str) {
+    return str.toLowerCase().replace(/[^a-z0-9]/gi, "");
+}
+
+function isPalindrome(str) {
+    const cleaned = normalize(str);
+    const reversed = [...cleaned].reverse().join(""); // [...]: spread operator
+    return cleaned === reversed; // true or false
+}
+
+function handleCheck() {
+    const original = inputEl.value.trim();
+    if (!original) { // equals to if (original === "") // falsy in javascript: "", 0, null, undefined, false
         alert("Please input a value.");
         return;
     }
-})
+
+    const ok = isPalindrome(original);
+    resultEl.textContent = `${original} is ${ok ? "" : "not "}a palindrome.`
+}
+
+checkBtn.addEventListener("click", handleCheck);
