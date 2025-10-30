1707a0ec94a140bdd05577a0b6df7326bcd4fb29
Idra Hsieh <idra.hsieh@gmail.com>
2025-10-30 22:24:33 +0800

    feat(react:01-accordion): set up base component and connect data source

--- react/src/components/01-accordion/index.jsx
+++ react/src/components/01-accordion/index.jsx
@@ -1,5 +1,33 @@
+// single selection
+// multiple selection
+
+import { useState } from "react";
+import data from "./data";
+
 export default function Accordion() {
-    return <div>
-        Accordion
-    </div>
+    const [selected, setSelected] = useState(null);
+
+    return (
+        <div className="wrapper">
+            <div className="accordion">
+                {data && data.length > 0 ? (
+                    data.map((dataItem, index) => (
+                        <div key={index} className="item">
+                            <div className="title" onClick={() => setSelected(index)}>
+                                <h3>{dataItem.question}</h3>
+                                <span>{selected === index ? "–" : "+"}</span>
+                            </div>
+                            {selected === index && (
+                                <div className="content">
+                                    <p>{dataItem.answer}</p>
+                                </div>
+                            )}
+                        </div>
+                    ))
+                ) : (
+                    <div>No data found.</div>
+                )}
+            </div>
+        </div>
+    );
 }
