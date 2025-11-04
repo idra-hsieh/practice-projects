c1ccc7f68197dbc9c11b6143105b15ffb677a685
Idra Hsieh <idra.hsieh@gmail.com>
2025-11-04 14:40:29 +0800

    feat(fCC-js-02-roman-num): add pure greedy convertToRoman implementation

--- freeCodeCamp/02-javascript-algorithms-and-data-structures/02-roman-numerical-converter/script.js
+++ freeCodeCamp/02-javascript-algorithms-and-data-structures/02-roman-numerical-converter/script.js
@@ -47,5 +47,29 @@ function validateandConvert(n) {
 }
 
 function convertToRoman(n) {
-    return '';
+    const pairs = [
+        ['M', 1000],
+        ['CM', 900],
+        ['D', 500],
+        ['CD', 400],
+        ['C', 100],
+        ['XC', 90],
+        ['L', 50],
+        ['XL', 40],
+        ['X', 10],
+        ['IX', 9],
+        ['V', 5],
+        ['IV', 4],
+        ['I', 1],
+    ]
+    let res = '';
+    let remaining = n;
+    for (const [r, v] of pairs) {
+        while (remaining >= v) {
+            res += r;
+            remaining -= v;
+        }
+        if (remaining === 0) break;
+    }
+    return res;
 }
